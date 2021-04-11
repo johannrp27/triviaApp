8d25d11be6c77acf0da5be1784d384b19d1ca4a7
Johann Rojas <jrojas@modyo.com>
2021-04-11 18:02:24 -0500

    refactor(App): Modified base layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
+// import Header from './components/Header/Header'
 import MainPage from './components/MainPage'
 import './scss/App.scss'
 function App () {
   return (
     <div className="App">
-      <MainPage />
+      <div className="container h-100 d-flex flex-column justify-content-center align-items-center">
+        <div className="row w-100">
+          <div className="offset-sm-2 offset-lg-3 col-12 col-sm-8 col-lg-6 ">
+            <MainPage />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }

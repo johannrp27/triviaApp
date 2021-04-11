5d9da2a133264593f2497c62303eb1563457003e
Johann Rojas <jrojas@modyo.com>
2021-04-11 18:07:14 -0500

    refactor(Question): Created dummy questions and button

--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -1,19 +1,30 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 import AnswerButton from '../AnswerButton/AnswerButton'
-
+import Header from '../Header/Header'
+import './Question.scss'
 const Question = () => {
+  const { t } = useTranslation()
   return (
-    <div className="px-4 pt-5">
-      <div className="question h4 mb-4">
-        What is the largest organ of the human body?
+    <>
+      <Header/>
+      <div className="card data shadow-lg">
+        <div className="px-4 pt-5">
+          <div className="h4 mb-4 title">
+            What is the largest organ of the human body?
+          </div>
+          <div className="pb-4">
+            <AnswerButton answer={'Respuesta 1'}/>
+            <AnswerButton answer={'Respuesta 2'}/>
+            <AnswerButton answer={'Respuesta 3'}/>
+            <AnswerButton answer={'Respuesta 4'}/>
+          </div>
+          <div className="mb-3 text-end">
+            <button type="button" className="btn btn-primary common shadow">{t('next')}</button>
+          </div>
+        </div>
       </div>
-      <div>
-        <AnswerButton answer={'Respuesta 1'}/>
-        <AnswerButton answer={'Respuesta 2'}/>
-        <AnswerButton answer={'Respuesta 3'}/>
-        <AnswerButton answer={'Respuesta 4'}/>
-      </div>
-    </div>
+    </>
   )
 }
 

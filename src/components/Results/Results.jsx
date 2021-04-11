a67714e09b165a0add51376bec2f23f3401417eb
Johann Rojas <jrojas@modyo.com>
2021-04-11 18:08:03 -0500

    refactor(Results): Updated header called without image

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 import winnerLogo from '../../static/icons/winner.svg'
+import Header from '../Header/Header'
 import './Results.scss'
 const Results = ({ results = 0 }) => {
   const { t } = useTranslation()
 
   return (
-    <div className="px-4 pt-5 text-center">
-      <div className="mb-4 results">
-        <img className="pb-5" src={winnerLogo} alt="Winner logo"/>
-        <div className="py-3 display-6 bold">{t('result.name')}</div>
-        <div className="pb-5">{t('result.t1')} <span className="correctAnswers">{results}</span> {t('result.t2')} </div>
-        <button type="button" className="btn btn-again">{t('tryAgain')}</button>
+    <>
+      <Header activeImage={false}/>
+      <div className="card data shadow-lg">
+        <div className="px-4 pt-5 text-center">
+          <div className="mb-4 results">
+            <img className="pb-5" src={winnerLogo} alt="Winner logo"/>
+            <div className="py-3 display-6 bold">{t('result.name')}</div>
+            <div className="pb-5">{t('result.t1')} <span className="correctAnswers">{results}</span> {t('result.t2')} </div>
+            <button type="button" className="btn btn-again">{t('tryAgain')}</button>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 

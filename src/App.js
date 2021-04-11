116624081d531516067be5270d076ed74496e9f0
Johann Rojas <jrojas@modyo.com>
2021-04-10 21:51:02 -0500

    feat: Created Main Page and some styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import MainPage from './components/MainPage';
-
-function App() {
+import MainPage from './components/MainPage'
+import './scss/App.scss'
+function App () {
   return (
     <div className="App">
       <MainPage />
@@ -9,4 +9,4 @@ function App() {
   )
 }
 
-export default App;
+export default App
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import Welcome from './Welcome/Welcome'
+import logo from '../static/icons/triviaPerson.svg'
+import './MainPage.scss'
 
 const MainPage = () => {
   return (
-    <div>
-      Hello
+    <div className="container h-100">
+      <div className="d-flex h-100 justify-content-center align-items-center">
+        <div className="row">
+          <div className="col-8">
+            <div className="h1 text-white">TRIVIA GAME</div>
+          </div>
+          <div className="col-4 position-relative">
+            <img className="img-fluid position-absolute trivia" src={logo} alt="Person Trivia" />
+          </div>
+          <div className="col-12">
+            <div className="card shadow-lg">
+              <Welcome />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }

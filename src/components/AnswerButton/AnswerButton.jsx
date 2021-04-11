e38721b3279c738d78014f27b51c9955b218794c
Johann Rojas <jrojas@modyo.com>
2021-04-11 18:05:42 -0500

    fix: corrected minor styles

--- src/components/AnswerButton/AnswerButton.jsx
+++ src/components/AnswerButton/AnswerButton.jsx
@@ -4,7 +4,7 @@ import './AnswerButton.scss'
 const AnswerButton = ({ answer }) => {
   return (
     <>
-      <div type="button" className="btn answer w-100 mb-3">
+      <div type="button" className="btn answer w-100 mb-3 py-2">
         <div className="row">
           <div className="col-2 lead m-0">A</div>
           <div className="col-10 lead m-0 text-start">{answer}</div>

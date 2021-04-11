8592addb12f4a514b075bb37e4e5c201c0c71f5b
Johann Rojas <jrojas@modyo.com>
2021-04-11 12:43:32 -0500

    feat: Created Answer Button Dummy

---
+++ src/components/AnswerButton/AnswerButton.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './AnswerButton.scss'
+
+const AnswerButton = ({ answer }) => {
+  return (
+    <>
+      <div type="button" className="btn answer w-100 mb-3">
+        <div className="row">
+          <div className="col-2 lead m-0">A</div>
+          <div className="col-10 lead m-0 text-start">{answer}</div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default AnswerButton

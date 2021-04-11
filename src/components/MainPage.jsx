82a902b40a9bfc512a47e6f159a5189eea0e11fd
Johann Rojas <jrojas@modyo.com>
2021-04-11 12:39:31 -0500

    refactor(MainPage): Modified layout of card to work responsive

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,23 +1,24 @@
 import React from 'react'
-import Welcome from './Welcome/Welcome'
+// import Welcome from './Welcome/Welcome'
 import logo from '../static/icons/triviaPerson.svg'
 import './MainPage.scss'
+import Question from './Question/Question'
 
 const MainPage = () => {
   return (
-    <div className="container h-100">
-      <div className="d-flex h-100 justify-content-center align-items-center">
-        <div className="row">
-          <div className="col-8">
-            <div className="h1 text-white">TRIVIA GAME</div>
-          </div>
-          <div className="col-4 position-relative">
-            <img className="img-fluid position-absolute trivia" src={logo} alt="Person Trivia" />
-          </div>
-          <div className="col-12">
-            <div className="card shadow-lg">
-              <Welcome />
+    <div className="container h-100 d-flex flex-column justify-content-center align-items-center">
+      <div className="row w-100">
+        <div className="offset-sm-2 offset-lg-3 col-12 col-sm-8 col-lg-6 ">
+          <div className="header">
+            <div >
+              <div className="h1 text-white">TRIVIA GAME</div>
             </div>
+            <div className="position-relative">
+              <img className="img-fluid position-absolute trivia" src={logo} alt="Person Trivia" />
+            </div>
+          </div>
+          <div className="card shadow-lg">
+            <Question />
           </div>
         </div>
       </div>

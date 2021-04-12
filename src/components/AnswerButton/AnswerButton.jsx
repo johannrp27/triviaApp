f46dc89c5649443be362f034b17b1bcfe119946e
Johann Rojas <jrojas@modyo.com>
2021-04-12 12:35:43 -0500

    feat(AnswerButton): Updated dynamic letter of answer button

--- src/components/AnswerButton/AnswerButton.jsx
+++ src/components/AnswerButton/AnswerButton.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './AnswerButton.scss'
 
-const AnswerButton = ({ answer }) => {
+const AnswerButton = ({ letter, answer }) => {
   return (
     <>
       <div type="button" className="btn answer w-100 mb-3 py-2">
         <div className="row">
-          <div className="col-2 lead m-0">A</div>
+          <div className="col-2 lead m-0">{String.fromCharCode(letter)}</div>
           <div className="col-10 lead m-0 text-start">{answer}</div>
         </div>
       </div>

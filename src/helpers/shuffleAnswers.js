ddf9312f3d30fcd6602a13aa7cab79bcb9bd5eeb
Johann Rojas <jrojas@modyo.com>
2021-04-17 14:23:33 -0500

    fix: Fixed answers no trimmed

--- src/helpers/shuffleAnswers.js
+++ src/helpers/shuffleAnswers.js
@@ -1,6 +1,6 @@
 
 const shuffleAnswers = (wrongAnswers = [], correctAnswer = '') => {
-  const array = [...wrongAnswers, correctAnswer]
+  const array = [...wrongAnswers, correctAnswer.trim()]
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]]

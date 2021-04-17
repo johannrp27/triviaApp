370e9777aad1098e3984d5fd4a5c26fbdfb33cec
Johann Rojas <jrojas@modyo.com>
2021-04-17 14:26:02 -0500

    feat: Decoded special chars from API response

--- src/helpers/getQuestions.js
+++ src/helpers/getQuestions.js
@@ -1,17 +1,32 @@
+import shuffleAnswers from './shuffleAnswers'
 import axios from 'axios'
 
-const BASE_URL = 'https://opentdb.com/api.php?'
+export const getQuestions = async (user) => {
+  let params = `amount=${user.amount}`
+  params += user.difficulty !== 'any' ? `&difficulty=${user.difficulty}` : ''
+  params += user.type !== 'any' ? `&type=${user.type}` : ''
+  const URL = `https://opentdb.com/api.php?${params}`
+  const { data } = await axios.get(URL)
 
-const fetchQuestions = async (user) => {
-  try {
-    let params = `amount=${user.amount}`
-    params += user.difficulty !== 'any' ? `&difficulty=${user.difficulty}` : ''
-    params += user.type !== 'any' ? `&type=${user.type}` : ''
-    const { data } = await axios.get(`${BASE_URL}${params}`)
-    return data.results
-  } catch (error) {
-    return error
+  if (data.response_code === 0) {
+    const questions = data.results.map((question, id) => {
+      question.incorrect_answers.forEach(ans => decodeHTML(ans))
+      const shuffledAnswers = shuffleAnswers(question.incorrect_answers, decodeHTML(question.correct_answer))
+      return {
+        id,
+        category: question.category,
+        question: decodeHTML(question.question),
+        correctAnswer: question.correct_answer,
+        answers: shuffledAnswers
+      }
+    })
+    return questions
   }
+  return 'Cannot fetch questions, there is something wrong in url'
 }
 
-export default fetchQuestions
+const decodeHTML = (text) => {
+  const textArea = document.createElement('textarea')
+  textArea.innerHTML = text
+  return textArea.value
+}

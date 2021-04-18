cc43279f5457d07256ec4ade3519085a4b567edf
Johann Rojas <jrojas@modyo.com>
2021-04-18 14:45:24 -0500

    style: Added spacing to loading screen

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
-import Header from '../Header/Header'
 
 const Loader = () => {
   return (
-    <>
-      <Header/>
-      <div className="card data shadow-lg">
-        <div className="d-flex justify-content-center">
-          <div className="spinner-border" style={{ width: 3 + 'rem', height: 3 + 'rem' }} role="status">
-            <span className="visually-hidden">Loading...</span>
-          </div>
+    <div className="card data shadow-lg">
+      <div className="d-flex justify-content-center py-5">
+        <div className="spinner-border my-5"
+          style={
+            {
+              width: 5 + 'rem',
+              height: 5 + 'rem',
+              border: 0.5 + 'rem solid #5256a1',
+              borderRightColor: 'transparent'
+            }}
+          role="status">
+          <span className="visually-hidden">Loading...</span>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 

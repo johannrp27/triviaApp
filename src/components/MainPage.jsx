3668384c04c905e2316a37c92b3303e3a234664a
Johann Rojas <jrojas@modyo.com>
2021-04-18 14:46:22 -0500

    perf: Modified layout base, cleaner

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,22 +1,16 @@
 import React from 'react'
-import Welcome from './Welcome/Welcome'
-import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
-import Question from './Question/Question'
-import Results from './Results/Results'
+import Navigation from './Navigation/Navigation'
 import './MainPage.scss'
-import Page404 from './404/Page404'
 
 const MainPage = () => {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Welcome}/>
-        <Route exact path="/question/:id" component={Question}/>
-        <Route exact path="/results" component={Results}/>
-        <Route exact path="/404" component={Page404}/>
-        <Redirect to="/404"/>
-      </Switch>
-    </Router>
+    <div className="container h-100 d-flex flex-column justify-content-center align-items-center">
+      <div className="row w-100">
+        <div className="offset-sm-2 offset-lg-3 col-12 col-sm-8 col-lg-6 ">
+          <Navigation/>
+        </div>
+      </div>
+    </div>
   )
 }
 

26f92e4e885d154f77a801ca47692dc16a17f6ee
Johann Rojas <jrojas@modyo.com>
2021-04-18 15:58:27 -0500

    fix: Header displayed on 404

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = ({ location }) => {
       <div >
         <div className="h1 text-white bold">TRIVIA GAME</div>
       </div>
-      <div className={location.pathname.includes('results') ? 'd-none' : 'position-relative'}>
+      <div className={location.pathname.search(/404|results/gmi) >= 0 ? 'd-none' : 'position-relative'}>
         <img className="img-fluid position-absolute trivia" id="img-header" src={logo} alt="Person Trivia" />
       </div>
     </div>

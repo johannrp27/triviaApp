74d15ba0883f349ec141ab5fd16f968f05119c64
Johann Rojas <jrojas@modyo.com>
2021-04-18 14:47:22 -0500

    refactor: Removed header and card style

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -3,27 +3,21 @@ import { useTranslation } from 'react-i18next'
 import { Link } from 'react-router-dom'
 import TriviaContext from '../../context/context'
 import winnerLogo from '../../static/icons/winner.svg'
-import Header from '../Header/Header'
 import './Results.scss'
 const Results = () => {
   const { t } = useTranslation()
   const { user: { correct } } = useContext(TriviaContext)
   return (
-    <>
-      <Header activeImage={false}/>
-      <div className="card data shadow-lg">
-        <div className="px-4 pt-5 text-center">
-          <div className="mb-4 results">
-            <img className="pb-5" src={winnerLogo} alt="Winner logo"/>
-            <div className="py-3 display-6 bold">{t('result.name')}</div>
-            <div className="pb-5">{t('result.t1')} <span className="correctAnswers">{correct || 0}</span> {t('result.t2')} </div>
-            <Link to={'/'}>
-              <button type="button" className="btn btn-again">{t('tryAgain')}</button>
-            </Link>
-          </div>
-        </div>
+    <div className="px-4 pt-5 text-center">
+      <div className="mb-4 results">
+        <img className="pb-5" src={winnerLogo} alt="Winner logo"/>
+        <div className="py-3 display-6 bold">{t('result.name')}</div>
+        <div className="pb-5">{t('result.t1')} <span className="correctAnswers">{correct || 0}</span> {t('result.t2')} </div>
+        <Link to={'/'}>
+          <button type="button" className="btn btn-again">{t('tryAgain')}</button>
+        </Link>
       </div>
-    </>
+    </div>
   )
 }
 

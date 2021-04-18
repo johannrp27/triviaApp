9b334866b5d8056b3621660b175b8a13b060b18b
Johann Rojas <jrojas@modyo.com>
2021-04-18 14:45:51 -0500

    perf: Unique header inside router

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -4,17 +4,21 @@ import Welcome from '../Welcome/Welcome'
 import Question from '../Question/Question'
 import Results from '../Results/Results'
 import Page404 from '../404/Page404'
+import Header from '../Header/Header'
 
 const Navigation = () => {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Welcome}/>
-        <Route exact path="/question/:id" component={Question}/>
-        <Route exact path="/results" component={Results}/>
-        <Route exact path="/404" component={Page404}/>
-        <Redirect to="/404"/>
-      </Switch>
+      <Header/>
+      <div className="card data shadow-lg">
+        <Switch>
+          <Route exact path="/" component={Welcome}/>
+          <Route exact path="/question/:id" component={Question}/>
+          <Route exact path="/results" component={Results}/>
+          <Route exact path="/404" component={Page404}/>
+          <Redirect to="/404"/>
+        </Switch>
+      </div>
     </Router>
   )
 }

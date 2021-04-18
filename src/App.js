16188615baaacc3d86cac0badb0cc1eec658fcda
Johann Rojas <jrojas@modyo.com>
2021-04-17 20:41:10 -0500

    feat: Modified initial state and optimization

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ import TriviaContext from './context/context'
 import './scss/App.scss'
 function App () {
   const initialState = {
-    name: '',
-    amount: 5,
-    difficulty: 'any',
-    type: 'any',
     questions: [],
     correct: 0
   }
@@ -15,13 +11,7 @@ function App () {
   return (
     <TriviaContext.Provider value={{ user, setUser, initialState }}>
       <div className="App">
-        <div className="container h-100 d-flex flex-column justify-content-center align-items-center">
-          <div className="row w-100">
-            <div className="offset-sm-2 offset-lg-3 col-12 col-sm-8 col-lg-6 ">
-              <MainPage />
-            </div>
-          </div>
-        </div>
+        <MainPage />
       </div>
     </TriviaContext.Provider>
   )

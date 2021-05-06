ee232b254919559116f1a33a9c28e7d4fa686f57
Johann Rojas <jrojas@modyo.com>
2021-05-06 14:32:37 -0500

    fix: Answer buttons now refresh on every question

--- src/components/AnswerButton/AnswerButton.jsx
+++ src/components/AnswerButton/AnswerButton.jsx
@@ -3,12 +3,12 @@ import './AnswerButton.scss'
 import correctIcon from '../../static/icons/correctAnswer.svg'
 import wrongIcon from '../../static/icons/wrongAnswer.svg'
 
-const AnswerButton = ({ addPoint, isClicked, setClicked, correctAnswer, answer, id }) => {
+const AnswerButton = ({ addPoint, isClicked, setClicked, correctAnswer, answer, id, letter }) => {
   const [style, setStyle] = useState('notAnswered')
 
   useEffect(() => {
     setStyle('notAnswered')
-  }, [answer])
+  }, [id])
 
   const handleClick = () => {
     const isCorrect = answer === correctAnswer
@@ -24,7 +24,7 @@ const AnswerButton = ({ addPoint, isClicked, setClicked, correctAnswer, answer, 
   return (
     <div type="button" onClick={handleClick} className={`btn radius w-100 mb-3 py-2 ${style} ${isClicked ? 'disabled' : ''}`}>
       <div className="row d-flex align-items-center">
-        <div className="col-2 lead m-0">{String.fromCharCode(65 + id)}</div>
+        <div className="col-2 lead m-0">{String.fromCharCode(65 + letter)}</div>
         <div className="col-8 lead m-0 text-start">{answer}</div>
         <div className="col-2 m-0 text-center">
           { isClicked
